9fec3003f0ee2f87691a2a6235657991298eddf6
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-02 16:41:57 +0100

    finished polishing css for form

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,23 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// FORM JAVASCRIPT
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
